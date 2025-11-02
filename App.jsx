8eeeecbfb278e90daffde9367ae541c92d33c60f
pushfirst <shakti.srivastava0509@gmail.com>
2025-11-02 20:15:58 +0530

    chore: element inside component

--- App.jsx
+++ App.jsx
@@ -2,22 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const number = 1;
-const TitleComponent = () => (
-  <h1 className="tile" tabIndex="1">
-    I am a title functional component {number}
-  </h1>
-);
+
 // component composition
 const HeadingComponent = () => {
   return (
     <div id="container">
-      <TitleComponent />
       <h1 className="head" tabIndex="1">
         I am h1 heading functional component
       </h1>
     </div>
   );
 };
+const TitleElement = (
+  <h1 className="tile" tabIndex="1">
+    I am a title functional component {number}
+    <HeadingComponent />
+  </h1>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(TitleElement);
